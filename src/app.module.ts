8401153662157064b50e2bcb1a6c46e99f7ac0e3
Vinícius do Vale <viniciusdovale@MacBook-Pro-de-Vinicius.local>
2024-09-05 18:04:27 -0300

    feat: adjust env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'aws-0-sa-east-1.pooler.supabase.com',
-      port: 6543,
-      username: 'postgres.sczqutwagujgbroddydr',
-      password: 'BBFkmck8JGBy152C',
-      database: 'postgres',
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT, 10),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
     }),
