ed2b99e180e5f1b7a40986c2b10067f9b610f350
Vinícius do Vale <viniciusdovale@MacBook-Pro-de-Vinicius.local>
2024-09-05 18:44:39 -0300

    feat: adjust test typeorm

--- src/products/products.controller.spec.ts
+++ src/products/products.controller.spec.ts
@@ -1,36 +1,52 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ProductsController } from './products.controller';
+import { ProductController } from './products.controller';
 import { ProductsService } from './products.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ProductEntity } from './entities/product.entity';
+import { Repository } from 'typeorm';
 
 describe('ProductsController', () => {
-  let controller: ProductsController;
+  let controller: ProductController;
   let service: ProductsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [ProductsController],
-      providers: [ProductsService],
+      controllers: [ProductController],
+      providers: [
+        ProductsService,
+        {
+          provide: getRepositoryToken(ProductEntity),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
-    controller = module.get<ProductsController>(ProductsController);
+    controller = module.get<ProductController>(ProductController);
     service = module.get<ProductsService>(ProductsService);
   });
 
   it('should return an array of products', async () => {
-    const result = [
+    const result: ProductEntity[] = [
       {
         id: 1,
-        name: 'Product 1',
         code: 'P001',
+        name: 'Product 1',
         available: true,
-        full_price_in_cents: 10000,
-        sale_price_in_cents: 9000,
+        visible: true,
+        details: {
+          name: 'Product 1',
+          description: 'Description of Product 1',
+        },
+        fullPriceInCents: '10000',
+        salePriceInCents: '9000',
         rating: 4.5,
+        image: 'https://example.com/product-image.jpg',
+        stockAvailable: true,
       },
     ];
 
-    jest.spyOn(service, 'findAll').mockImplementation(async () => result);
+    jest.spyOn(service, 'getAllProducts').mockResolvedValue(result);
 
-    expect(await controller.findAll()).toBe(result);
+    expect(await controller.getAllProducts()).toBe(result);
   });
 });
--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -1,18 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ProductService } from './products.service';
+import { ProductsService } from './products.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ProductEntity } from './entities/product.entity';
 
 describe('ProductsService', () => {
   let service: ProductsService;
+  let repository: Repository<ProductEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        {
+          provide: getRepositoryToken(ProductEntity),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
+    repository = module.get<Repository<ProductEntity>>(
+      getRepositoryToken(ProductEntity),
+    ); // Obtém o repositório simulado
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return an array of products', async () => {
+    const result: ProductEntity[] = [
+      {
+        id: 1,
+        code: 'P001',
+        name: 'Product 1',
+        available: true,
+        visible: true,
+        details: {
+          name: 'Product 1',
+          description: 'Description of Product 1',
+        },
+        fullPriceInCents: '10000',
+        salePriceInCents: '9000',
+        rating: 4.5,
+        image: 'https://example.com/product-image.jpg',
+        stockAvailable: true,
+      },
+    ];
+
+    jest.spyOn(repository, 'find').mockResolvedValue(result);
+
+    expect(await service.getAllProducts()).toEqual(result);
   });
 });
